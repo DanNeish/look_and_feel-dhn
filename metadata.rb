16cabd75fa25856fe3ba9362f6bcc1d5360ad555
Daniel Neish <daniel.neish@gmail.com>
2017-01-23 09:27:19 +1100

    Bump revision

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "look_and_feel-dhn"
 maintainer        "Daniel Neish"
 maintainer_email  "daniel.neish@gmail.com"
 description       "Installs tools to make the server nicer to work on, quite opinionated"
-version           "0.0.8"
+version           "0.0.9"
 
 recipe "look_and_feel-dhn", "Adds visual flag to production environment as well as htop, vim and zip, also allows for the installation of additional locales"
 
